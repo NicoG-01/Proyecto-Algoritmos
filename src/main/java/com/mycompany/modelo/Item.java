b30fde3f826906ad9d5a64b169b1b96077043451
josephdgj4k <josephdgj@gmail.com>
2026-04-21 15:25:05 -0500

    Se añade clase ITEM y JUEGO

---
+++ src/main/java/com/mycompany/modelo/Item.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo;
+
+/**
+ *
+ * @author JosephDG
+ */
+public class Item {
+    private int posicion_x;
+    private int posicion_y;
+    private char tipo;    
+}
---
+++ src/main/java/com/mycompany/modelo/Juego.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo;
+
+/**
+ *
+ * @author JosephDG
+ */
+public class Juego {
+    private Mapa mapa;
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private int tiempo;
+    private char estado;   
+}
