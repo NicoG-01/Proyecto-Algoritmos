e53d9a4079e2ea5e89e6bf5d17ae2de04dd9a544
NicoG-01 <diegon.garzons@utadeo.edu.co>
2026-04-22 22:05:53 -0500

    Se ajusta editorController y vista de controlador

--- src/main/java/com/mycompany/vista/EditorController.java
+++ src/main/java/com/mycompany/vista/EditorController.java
@@ -4,15 +4,16 @@
  */
 package com.mycompany.vista;
 
+import com.mycompany.modelo.Celda;
+import com.mycompany.modelo.Mapa;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 /**
  * FXML Controller class
@@ -20,13 +21,48 @@ import javafx.stage.Stage;
  * @author diego
  */
 public class EditorController implements Initializable {
-
+    private Mapa mapa;
+    private Canvas canvas;
+    private GraphicsContext gc;
+    
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private BorderPane vistaEditor;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        mapa = new Mapa(11, 13, "Mapa-Dev");
+        canvas = new Canvas(mapa.getColumnas()*48, mapa.getFilas()*48);
+        gc = canvas.getGraphicsContext2D();
+        vistaEditor.setCenter(canvas);
+        dibujarMapa();
         // TODO
     }    
+    
+    private void dibujarMapa(){
+        for (int f = 0; f < mapa.getFilas(); f++){
+            for (int c = 0; c < mapa.getColumnas(); c ++) {
+                Celda celda = mapa.getCelda(f, c);
+                
+                switch (celda.getTipoCelda()) {
+                    case VACIO:
+                        gc.setFill(Color.LIGHTGRAY);
+                        break;
+                    case PARED_FIJA:
+                        gc.setFill(Color.DARKGRAY);
+                        break;
+                    case PARED_DESTRUCTIBLE:
+                        gc.setFill(Color.BROWN);
+                        break;
+                }
+                gc.fillRect(c * 48, f * 48, 48, 48);
+                
+                gc.setStroke(Color.BLACK);
+                gc.strokeRect(c * 48 , f * 48, 48, 48);
+            }
+        }
+    }
     
 }
