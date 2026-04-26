3a8838f990a9a44eaef6ac6ee3de1286cb81493e
josephdgj4k <josephdgj@gmail.com>
2026-04-26 16:04:21 -0500

    Se adiciona metodos clase jugador

--- src/main/java/com/mycompany/modelo/Jugador.java
+++ src/main/java/com/mycompany/modelo/Jugador.java
@@ -18,6 +18,7 @@ public class Jugador {
     private int vida;
     private int max_bomba;
     private int rango_bomba;
+    private boolean vivo;
     
     public Jugador(){
         this.posicion_x = -1;
@@ -28,6 +29,7 @@ public class Jugador {
         this.vida = 5;
         this.max_bomba = 1;
         this.rango_bomba = 1;
+        this.vivo = true;
     }
     
     public int getVelocidad(){
@@ -53,4 +55,63 @@ public class Jugador {
     public void setMaxBomba(int pNumeroBombas){
         max_bomba = pNumeroBombas;
     }
+    
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    public int getPosicionX() {
+        return posicion_x;
+    }
+
+    public void setPosicionX(int x) {
+        this.posicion_x = x;
+    }
+
+    public int getPosicionY() {
+        return posicion_y;
+    }
+
+    public void setPosicionY(int y) {
+        this.posicion_y = y;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void cogerItem(Item ventaja){
+        ventaja.aplicarEfecto(this);               
+    }
+    
+    public void mover(int nuevo_posicionx, int nuevo_posiciony, Mapa mapa){
+        int nuevox = getPosicionX()+ nuevo_posicionx;
+        int nuevoy = getPosicionY()+ nuevo_posiciony;
+        
+        if (nuevox <0 || nuevox >mapa.getColumnas()) return;
+        if (nuevoy <0 || nuevoy >mapa.getFilas()) return;
+        
+        TipoCelda tipo = mapa.getCelda(nuevoy, nuevox).getTipoCelda();
+        
+        if (tipo == TipoCelda.PARED_DESTRUCTIBLE) return;
+        if (tipo == TipoCelda.PARED_FIJA)return;
+        
+        setPosicionX(nuevox);
+        setPosicionY(nuevoy);
+        
+        Celda celda = mapa.getCelda(nuevoy, nuevox);
+        if (celda.tieneItem()){
+            cogerItem(celda.getItem());
+            celda.setItem(null);
+        }    
+    }                
 }
+
