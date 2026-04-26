af5e4765964d1f5df447821b884b9271aa03a957
Danigj-star <angied.garciaj@utadeo.edu.co>
2026-04-26 11:43:22 -0500

    Añadiendo metodos get/set en Juego

--- src/main/java/com/mycompany/modelo/Juego.java
+++ src/main/java/com/mycompany/modelo/Juego.java
@@ -14,4 +14,28 @@ public class Juego {
     private Jugador jugador2;
     private int tiempo;
     private char estado;   
+    public Mapa getMapa() {
+        return mapa;
+    }
+// // Retorna el primer jugador de la partida
+    public Jugador getJugador1() {
+        return jugador1;
+    }
+// // Retorna el Segundo jugador de la partida
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+ // Retorna el tiempo transcurrido o restante del juego
+    public int getTiempo() {
+        return tiempo;
+    }
+ // Retorna el estado actual del juego (I=Iniciado, P=Pausado, F=Finalizado)
+    public char getEstado() {
+        return estado;
+    }
+// Cambia el estado actual del juego
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+   
 }
